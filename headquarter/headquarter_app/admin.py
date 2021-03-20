ac72e75cf133a49c9646d681681738a59ae6ca9a
amrabouelenin <amr@new-track.net>
2021-03-20 09:39:52 +0000

    database migrations files and models

--- headquarter/headquarter_app/admin.py
+++ headquarter/headquarter_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from headquarter_app.models import Branch
+
+admin.site.register(Branch)
 
 # Register your models here.
--- headquarter/headquarter_app/models.py
+++ headquarter/headquarter_app/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Branch(models.Model):
+    name = models.CharField(max_length=264, unique= True, help_text = "Please write the name of branch") 
