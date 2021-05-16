d74c661dd86c4ae1f072e7ad6264a22e1a79a583
amroabouelenin <amr@New-track.net>
2021-05-16 16:04:31 +0200

    update clietns

--- clients/send_revenue.py
+++ clients/send_revenue.py
@@ -1,44 +1,81 @@
 # importing the requests library
 import requests, json
+from get_revenue import GetRevenue
+from datetime import date
+## Read the day of today amount
+########################################### RUN DAILY cron.daily #####################################
+# inistanitiate service
+G = GetRevenue()
 
-# First authenticate in order to use the required APIs, then call the targeted service api
+# call service
+G.get_reveneu()
 
-# API Endpoint
-API_ENDPOINT = "http://127.0.0.1:8000/api/"
+# store data to be sent
+data = G.data
 
-# Credentials of api user
-credentials = { 'username' : 'testapi', 'password' : 'testapi' }
+## handle Fault Tolerence
+try:
 
-# service path
-api_url = "token/"
+    # First authenticate in order to use the required APIs, then call the targeted service api
 
-# authenticate and get the crt Token to be used later
-request = requests.post(API_ENDPOINT + api_url, data=credentials, verify=False)
+    # API Endpoint
+    API_ENDPOINT = "http://127.0.0.1:8001/api/"
 
+    # Credentials of api user
+    credentials = { 'username' : 'testapi', 'password' : 'testapi' }
 
-# store Token
-token = json.loads(request.text)['token']
+    # service path
+    api_url = "token/"
 
-# display success message
-print("Authenticated with Token, ", token)
+    # authenticate and get the crt Token to be used later
+    request = requests.post(API_ENDPOINT + api_url, data=credentials, verify=False)
 
-# build the header dict to be passed with any request that requires authentication
-headers = { 'Authorization' : 'Token ' + token }
+    # store Token
+    token = json.loads(request.text)['token']
 
-# send revenue report
-# revenue report data  # alternative way to get the data is to access the database through another api call in the branch office
-data ={
-        "date": 1616242925,
-        "revenu": 3000,
-        "loses": 3000,
-        "Branch": 1
-      }
+    # display success message
+    print("Authenticated with Token, ", token)
 
-# revenue url
-api_url = 'send-revenue/'
+    # build the header dict to be passed with any request that requires authentication
+    headers = { 'Authorization' : 'Token ' + token }
 
-# send the report using POST
-request = requests.post(url = API_ENDPOINT + api_url, headers=headers, data = data)
+    # send revenue report
+    # revenue report data  # alternative way to get the data is to access the database through another api call in the branch office
 
-# print response
-print(json.loads(request.text))
+    # revenue url
+    api_url = 'send-revenue/'
+
+    # send the report using POST
+    request = requests.post(url = API_ENDPOINT + api_url, headers=headers, data = data)
+
+    # print response
+    print(json.loads(request.text))
+
+    calls_data =   {
+        "status": True,
+        "expected_date": date.today(),
+        "actual_date": date.today(),
+        "comments": "ran succesfully",
+        "revenue": G.data['revenu'],
+        "loss": G.data['loses']
+    }
+
+    message = G.set_request_status(calls_data)
+
+    # message = G.set_request_status(calls_data)
+    print("Headquarter is up",message)
+except:
+    ## add status record equal zero
+
+    calls_data =   {
+        "status": False,
+        "expected_date": date.today(),
+        "actual_date": None,
+        "comments": "didn't run",
+        "revenue": G.data['revenu'],
+        "loss": G.data['loses']
+    }
+
+    message = G.set_request_status(calls_data)
+    print("Headquarter is down",message)
+    pass
